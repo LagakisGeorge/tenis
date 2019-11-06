fec59ff9804cb348c5945f0569fe57a9d4864c90
george <glagakis@gmail.com>
2019-11-06 23:12:59 +0200

    afterTextChanged με την συμπληρωση 4 χαρακτηρων αναζητα τον πελατη αυτοματα

--- app/src/main/java/com/example/tenis/addPel.java
+++ app/src/main/java/com/example/tenis/addPel.java
@@ -6,9 +6,13 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteAccessPermException;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class addPel extends AppCompatActivity {
 
@@ -16,7 +20,59 @@ public class addPel extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_pel);
+        EditText mKOD;
+
+        mKOD = (EditText) findViewById(R.id.KOD);
+
+        mKOD.addTextChangedListener(new TextWatcher() {
+            public void afterTextChanged(Editable s) {
+
+              if (s.toString().length()==4) adding();
+              //  Toast.makeText(getApplicationContext(), "editable "+s.toString(), Toast.LENGTH_SHORT).show();
+
+            }
+
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                /*This method is called to notify you that, within s, the count characters beginning at start are about to be replaced by new text with length after. It is an error to attempt to make changes to s from this callback.*/
+            }
+
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+
+        });
     }
+
+
+   /* @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_6:
+                // Don't allow this value to go to 0. It shuts the screen off.
+                //if (minBrightness > 1) {
+               //     minBrightness -= 1;
+               //     updateDisplay();
+               //     Util.setActivityBrightness(getWindow(), minBrightness);
+               // }
+                Toast.makeText(getApplicationContext(), "keycode_6=13", Toast.LENGTH_SHORT).show();
+                return true;
+            case KeyEvent.KEYCODE_VOLUME_UP:
+                Toast.makeText(getApplicationContext(), "volume up", Toast.LENGTH_SHORT).show();
+                //if (minBrightness < 255) {
+                //    minBrightness += 1;
+                //    updateDisplay();
+                //    Util.setActivityBrightness(getWindow(), minBrightness);
+                //}
+                return true;
+            default:
+                Toast.makeText(getApplicationContext(), "default keycode_6=13", Toast.LENGTH_SHORT).show();
+                return super.onKeyDown(keyCode, event);
+        }
+    }
+*/
+
+
+
 
 
     public void CREATE(View view) {
@@ -60,7 +116,11 @@ public class addPel extends AppCompatActivity {
     }
 
 
-    public void ADD_PEL(View view) {
+    public void ADD_PEL(View v) {
+        adding();
+    }
+
+    public void adding() {
         SQLiteDatabase mydatabase = null;
         mydatabase = openOrCreateDatabase("pelates", MODE_PRIVATE, null);
         EditText mKOD, mONO;
