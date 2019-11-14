648b99eec0470bce5a6677892fd502da76eae60e
george <glagakis@gmail.com>
2019-11-15 00:23:53 +0200

    ok με τις ημερομηνιες δειχνει σε grid απενεργοποιησα email

--- app/src/main/java/com/example/tenis/kiniseis.java
+++ app/src/main/java/com/example/tenis/kiniseis.java
@@ -14,14 +14,19 @@ import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.GridView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class kiniseis extends AppCompatActivity {
 
@@ -34,8 +39,8 @@ public class kiniseis extends AppCompatActivity {
     CalendarView calendar;
     TextView dateView;
     int flag1 = 0;
-
-
+    String d1,d2;
+    public List<String> values=new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,13 +53,40 @@ public class kiniseis extends AppCompatActivity {
         calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-                String Date = dayOfMonth + "-" + (month + 1) + "-" + year;
+
+                String word,cMonth,cDay;
+
+                int mm=100+(month+1);
+                word=Integer.toString(mm) ;
+                cMonth=word.substring(word.length() - 2);
+
+                mm=100+(dayOfMonth);
+                word=Integer.toString(mm) ;
+                cDay=word.substring(word.length() - 2);
+
+                // String Date = dayOfMonth + "-" + (month + 1) + "-" + year;
+                String Date = cDay + "-" + cMonth + "-" + year;
+
                 if (flag1==0) {
-                    dateView.setText(Date);
+                    // dateView.setText(Date);
+                    d1= year+"-"+ cMonth +"-"+cDay  ;
+                    dateView.setText(d1);
                     flag1 = 1;
                 } else{
-                    t5.setText(Date);
+                    // t5.setText(Date);
+
+                    // d2= year+"/"+ (month + 1) +dayOfMonth  ;
+                    d2= year+"-"+ cMonth +"-"+cDay  ;
+                    t5.setText(d2);
                     calendar.setVisibility(View.INVISIBLE );
+                    EditText sql;
+                    sql=findViewById(R.id.editText);
+                    sql.setText("select IDBARDIA, CH1,CH2  from  parousies where CH1>='"+d1+"' AND CH1<='"+d2+"' AND CH2+CH1 NOT NULL order by CH1 desc");
+
+
+
+
+
                 }
             }
         });
@@ -133,7 +165,7 @@ public class kiniseis extends AppCompatActivity {
                     SQLiteDatabase db = null;
                     db = openOrCreateDatabase("pelates", MODE_PRIVATE, null);
 
-                    Cursor cursor = db.rawQuery("select IDBARDIA,CH1,CH2  from  parousies  order by CH1 desc", null);  //+ " order by CH1 desc"
+                    Cursor cursor = db.rawQuery("select IDBARDIA,CH1,CH2  from  parousies where CH1>='"+d1+"'  and CH1<='"+d2+"' order by CH1 desc", null);  //+ " order by CH1 desc"
 
                     // Cursor cursor = db.selectAll();
                     fw.append("ONOMA");
@@ -186,6 +218,19 @@ public class kiniseis extends AppCompatActivity {
 
 
 
+        values=new ArrayList<>();  // μηδενιζω την λιστα
+        values.add("Ονομα");
+        values.add("Ημ/νία");
+
+
+        EditText sql;
+        sql=findViewById(R.id.editText);
+        String SQL=sql.getText().toString();
+
+
+
+
+
 
         message="";
         try {
@@ -194,12 +239,14 @@ public class kiniseis extends AppCompatActivity {
             SQLiteDatabase db = null;
             db = openOrCreateDatabase("pelates", MODE_PRIVATE, null);
 
-            Cursor cursor = db.rawQuery("select IDBARDIA,CH1,CH2  from  parousies  order by CH1 desc", null);  //+ " order by CH1 desc"
+
+            Cursor cursor = db.rawQuery(SQL, null);  //+ " order by CH1 desc"
 
             if (cursor.moveToFirst()) {
                 do {
                     message=message+cursor.getString(2)+","+cursor.getString(0)+","+cursor.getString(1)+"\n";
-
+                    values.add(cursor.getString(2));
+                    values.add(cursor.getString(1));
                 } while (cursor.moveToNext());
             }
             if (cursor != null && !cursor.isClosed()) {
@@ -210,11 +257,16 @@ public class kiniseis extends AppCompatActivity {
         } catch (Exception e) {
         }
 
+        ArrayAdapter<String> arrayAdapter =
+                new ArrayAdapter<String>(kiniseis.this,android.R.layout.simple_list_item_1, values);
+
+        GridView moviesList;
+        moviesList=findViewById(R.id.grid);
+        moviesList.setAdapter(arrayAdapter);
 
 
 
-
-
+/*
 
 
 
@@ -247,7 +299,7 @@ public class kiniseis extends AppCompatActivity {
             File file = new File("/lagakis2/Test.csv");
             i.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file)); // data));
             i.putExtra(Intent.EXTRA_EMAIL, new String[]{to});
-            i.putExtra(Intent.EXTRA_SUBJECT, message);
+            i.putExtra(Intent.EXTRA_SUBJECT,"κινησεις");
             i.putExtra(Intent.EXTRA_TEXT, message);
             i.setData(Uri.parse("mailto:"));
 
@@ -257,6 +309,13 @@ public class kiniseis extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+
+
+
+
+
+ */
     }
 
 
